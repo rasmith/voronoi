e2283048fd29dd4974c3960634bdcc51d0f0b130
rhennig@gmail.com <>
2009-07-01 22:59:00 -0500

    site event modifications

--- src/rsmith/SiteEvent.java
+++ src/rsmith/SiteEvent.java
@@ -2,10 +2,19 @@ package rsmith;
 
 public class SiteEvent implements SweepEvent {
 
+	private Site site;
+	
 	@Override
 	public double getY() {
-		// TODO Auto-generated method stub
-		return 0;
+		return site.getPosition().getY();
+	}
+
+	public Site getSite() {
+		return site;
+	}
+
+	public void setSite(Site site) {
+		this.site = site;
 	}
 	
 }
