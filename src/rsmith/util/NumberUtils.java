ecacad3d80930324baf58324baed22a582267373
Randall Smith <rhennig@gmail.com>
2009-08-18 12:58:49 -0500

    changes

--- src/rsmith/util/NumberUtils.java
+++ src/rsmith/util/NumberUtils.java
@@ -1,9 +1,37 @@
 package rsmith.util;
 
 public class NumberUtils {
+	
+	public static double EPSILON = 10E-9;
+	
 	public static double randomNumber(double min, double max) {
 		double result = Math.random() * (max - min) + min;
 		// System.out.println("max="+max+"min="+min+",result="+result);
 		return result;
 	}
+	
+	public static boolean lt(double a, double b) {
+		return (a + NumberUtils.EPSILON < b);
+	}
+	
+	public static boolean gt(double a, double b) {
+		return (a - NumberUtils.EPSILON > b);
+	}
+	
+	public static boolean eq(double a, double b) {
+		return (Math.abs(b-a) < NumberUtils.EPSILON);
+	}
+	
+	public static boolean geq(double a, double b) {
+		return eq(a,b) || gt(a,b); 
+	}
+	
+	public static boolean  leq(double a, double b) {
+		return eq(a,b) || lt(a,b);
+	}
+	
+	public static boolean neq(double a, double b) {
+		return !eq(a,b);
+	}
+	
 }
--- src/rsmith/util/PointUtils.java
+++ src/rsmith/util/PointUtils.java
@@ -2,7 +2,13 @@ package rsmith.util;
 
 import java.awt.geom.Point2D;
 
+import rsmith.geom.Line;
+import rsmith.util.NumberUtils;
+
 public class PointUtils {
+	
+	public static Point2D ORIGIN = new Point2D.Double(0,0);
+	
 	public static int comparePointsX(Point2D p, Point2D q) {
 		return comparePoints(p, q, true);
 	}
@@ -25,4 +31,39 @@ public class PointUtils {
 		return new Point2D.Double(NumberUtils.randomNumber(min, max),
 				NumberUtils.randomNumber(min, max));
 	}
+	
+	public static Point2D subtract(Point2D p, Point2D q) {
+		return new Point2D.Double(p.getX()-q.getX(),p.getY()-q.getY());
+	}
+	
+	public static double left(Point2D p, Point2D q, Point2D r) {
+		Point2D v = subtract(q,p);
+		Point2D w = subtract(r,p);
+		return cross(v,w);
+	}
+	
+	public static boolean ccw(Line l, Point2D p) {
+		Line k1 = Line.create(ORIGIN,p);
+		Line k2 = l.perp(p);
+		Point2D q1 = l.intersect(k1);
+		Point2D q2 = l.intersect(k2);
+		return ccw(q1,q2,p);
+	}
+	
+	public static boolean ccw(Point2D p, Point2D q, Point2D r) {
+		return NumberUtils.gt(left(p,q,r),0);
+	}
+	
+	public static boolean cw(Point2D p, Point2D q, Point2D r) {
+		return NumberUtils.lt( left(p,q,r),0);
+	}
+	
+	public static boolean collinear(Point2D p, Point2D q, Point2D r) {
+		return NumberUtils.eq(left(p,q,r),0);
+	}
+	
+	public static double cross(Point2D p, Point2D q) {
+		return (p.getX()*q.getY() - p.getY()*q.getX());
+	}
+	
 }
